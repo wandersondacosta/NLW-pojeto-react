a0a2d02dbfdd14f31bda6d5d3cfbc4bd03b2d463
wanderson <wanderson_c_c@hotmail.com>
2020-08-05 19:43:34 -0300

    atualizando projeto

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -32,8 +32,8 @@ function Landing() {
             Estudar
           </a>
           <a href="" className="give-classes">
-            <img src={giveClassesIcon} alt="Estudar"/>
-            Estudar
+            <img src={giveClassesIcon} alt="Dar aula"/>
+            Dar aula
           </a>
         </div>
         <span className="total-connections">
