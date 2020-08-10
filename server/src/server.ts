f82ab502e2005b4437f4a0fbbb1c98d578000b86
wandersondacosta <wanderson_c_c@hotmail.com>
2020-08-09 21:44:03 -0300

    criando banco de dados com sqlite

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,21 @@
-console.log("Hello world");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+// GET: buscar ou listar uma informação.
+// POST: criar alguma nova infromção.
+// PUT: atualizar uma informção existente.
+// DELETE: deletear ou excluir uma iformação.
+
+// Corpo (request body): dados para criação ou atualização de um registro.
+
+// Route params: identificar qual recurso eu quero atualizar ou deletar.
+// Query params: paginação, filtros, ordenação.
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
+
+app.listen(3333);
