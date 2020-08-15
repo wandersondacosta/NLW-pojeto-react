c9d90979a9fd5b549c89a389e1f644e645f34a54
wanderson <wanderson_c_c@hotmail.com>
2020-08-15 08:27:55 -0300

    nova funcionalidades do sqlite

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,47 +1,9 @@
 import express from "express";
-import db from "./database/connection";
-import convertHourToMinutes from "./utils/convertHourToMinutes";
+import classesController from "./controllers/classesController";
 
 const routes = express.Router();
+const classesControllers = new classesController();
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
-
-routes.post("/classes", async (request, response) => {
-  const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
-
-  const insertedUsersIds = await db("users").insert({
-    name,
-    avatar,
-    whatsapp,
-    bio,
-  });
-
-  const user_id = insertedUsersIds[0];
-
-  const insertedClassesIds = await db("classes").insert({
-    subject,
-    cost,
-    user_id,
-  });
-
-  const class_id = insertedClassesIds[0];
-
-  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    return {
-      class_id,
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinutes(scheduleItem.from),
-      to: convertHourToMinutes(scheduleItem.to),
-    };
-  });
-
-  await db("class_schedule").insert(classSchedule);
-
-  return response.send();
-});
+routes.post("/classes");
 
 export default routes;
