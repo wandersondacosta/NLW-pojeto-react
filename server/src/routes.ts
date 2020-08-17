aef7e899738144621ddc784a1ad7824f50674a8a
wandersondacosta <wanderson_c_c@hotmail.com>
2020-08-16 21:09:56 -0300

    api finalils

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import classesController from "./controllers/classesController";
+import ConnectionController from "./controllers/connectionController";
 
 const routes = express.Router();
 const classesControllers = new classesController();
+const connectionsController = new ConnectionController();
 
-routes.post("/classes");
+routes.get("/classes", classesControllers.index);
+routes.post("/classes", classesControllers.create);
+
+routes.get("/connections", connectionsController.index);
+routes.post("/connections", connectionsController.create);
 
 export default routes;
